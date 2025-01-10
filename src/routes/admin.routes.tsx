0d49ec28ca2efeb082f7baa6ddb42b5aef20e16d
jonayeds <jonayeeds@gmail.com>
2025-01-10 14:45:56 +0600

    added routes genarator utils function

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -34,28 +34,25 @@ export const adminPaths = [
         ]
     },
 ]
-type TRoute = {
-    path:string,
-    element:ReactNode
-}
-export const adminRoutes = adminPaths.reduce((acc:TRoute[],item)=>{
-    if(item?.path && item?.element){
-        acc.push({
-            path:item.path,
-            element:item.element
-        })
-    }
-    if(item?.children){
-        item.children.forEach((child)=>{
-            acc.push({
-                path:child.path,
-                element:child.element
-            })
-        })
-        return acc
-    }
-    return acc
-},[])
+
+// export const adminRoutes = adminPaths.reduce((acc:TRoute[],item)=>{
+//     if(item?.path && item?.element){
+//         acc.push({
+//             path:item.path,
+//             element:item.element
+//         })
+//     }
+//     if(item?.children){
+//         item.children.forEach((child)=>{
+//             acc.push({
+//                 path:child.path,
+//                 element:child.element
+//             })
+//         })
+//         return acc
+//     }
+//     return acc
+// },[])
 
 type TSideBarItem = {
     key:string,
