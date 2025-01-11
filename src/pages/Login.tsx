74ea7139dac86820e1036f5675162fb9788f6c3d
jonayeds <jonayeeds@gmail.com>
2025-01-11 15:15:14 +0600

    connected to backend and handled cors issue

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,37 @@
+import { Button } from "antd"
+import { useForm } from "react-hook-form"
+import { useLoginMutation } from "../redux/features/auth/authApi"
 
 const Login = () => {
+  const {register, handleSubmit} = useForm({
+    defaultValues:{
+      id:"2024010001",
+      password:"123456"
+    }
+  })
+  const [login, {data,error}] = useLoginMutation()
+  console.log("data ==> ", data)
+  console.log("error ==> ", error)
+
+  const onSubmit = (data)=>{
+    const userInfo = {
+      id:data.id,
+      password:data.password
+    }
+    login(userInfo)
+  }
   return (
-    <div>Login</div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID</label>
+        <input type="text" id="id" {...register("id")}/>
+      </div>
+      <div>
+        <label htmlFor="Password">Password</label>
+        <input type="text" id="Password" {...register("password")} />
+      </div>
+      <Button htmlType="submit">Login</Button>
+    </form>
   )
 }
 
