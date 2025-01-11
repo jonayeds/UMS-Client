0124f6eb534eba79f3e6ca484a857d0acec04e7a
jonayeds <jonayeeds@gmail.com>
2025-01-11 20:44:56 +0600

    implemented login and fetched data from accessToken

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,24 +1,28 @@
 import { Button } from "antd"
 import { useForm } from "react-hook-form"
 import { useLoginMutation } from "../redux/features/auth/authApi"
+import { useAppDispatch } from "../redux/hooks"
+import { setUser } from "../redux/features/auth/authSlice"
+import { varifyToken } from "../utils/varifyToken"
 
 const Login = () => {
+  const dispatch = useAppDispatch()
   const {register, handleSubmit} = useForm({
     defaultValues:{
       id:"2024010001",
       password:"123456"
     }
   })
-  const [login, {data,error}] = useLoginMutation()
-  console.log("data ==> ", data)
-  console.log("error ==> ", error)
+  const [login, {error}] = useLoginMutation()
 
-  const onSubmit = (data)=>{
+  const onSubmit =async (data)=>{
     const userInfo = {
       id:data.id,
       password:data.password
     }
-    login(userInfo)
+    const res = await login(userInfo).unwrap()
+    const user = varifyToken(res.data.accessToken)
+    dispatch(setUser({user, token:res.data.accessToken}))
   }
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
